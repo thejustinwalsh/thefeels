dfe97bb26c717d97d535db7b31d937e1ed6ebd52
Justin Walsh <contact.me@thejustinwalsh.com>
2022-10-11 14:43:31 -0700

    feat: privacy & terms via MDX

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withMDX({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["avatars.githubusercontent.com", "platform-lookaside.fbsbx.com"],
   },
-};
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
 
 module.exports = nextConfig;
